4b53397cb3bbd93a497469ab8c585f43a553ba3f
MiltonC3 <miltoncoria03@gmail.com>
2024-01-03 20:17:44 -0300

    pagina productos corregida

--- src/pages/ProductsPage/ProductsPage.jsx
+++ src/pages/ProductsPage/ProductsPage.jsx
@@ -1,14 +1,10 @@
 // * Configurations
 import React from "react";
 
-// * Hooks
-import useProducts from "../../hooks/useProducts";
-
 // * Components
-import CardProduct from "../../components/CardProduct/CardProduct";
+import ListProducts from "../../components/ListProducts/ListProducts";
 
 const ProductsPage = () => {
-    const { products } = useProducts("/data/products.json");
     return (
         <div>
             <h1
@@ -20,20 +16,7 @@ const ProductsPage = () => {
             >
                 Todos los productos
             </h1>
-            <div className="div-productos" style={{ margin: "auto" }}>
-                {products.map((product) => {
-                    return (
-                        <div key={product.id}>
-                            <CardProduct
-                                id={product.id}
-                                nombre={product.nombre}
-                                precio={product.precio}
-                                imagen={product.imagen}
-                            />
-                        </div>
-                    );
-                })}
-            </div>
+            <ListProducts filter="all" />
         </div>
     );
 };
